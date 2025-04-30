ea019463f883fca80bdc17fa0e700ff5abe868ff
Reiad <reiadwc@gmail.com>
2025-04-29 22:18:10 -0400

    Fixed spelling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ function Home (){
                     Hello! I am <span className="name"> Reiad Sakoor</span>.
                     <br> 
                     </br>
-                    I am an aspiring full stack devleoper.
+                    I am an aspiring full stack developer.
                 </h1>
                 <a href="#About">
                     <button id="home-btn" className="btn">Explore Now</button>
