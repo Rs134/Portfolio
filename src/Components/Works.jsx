2d314d67bed4cd294c9f05c8ae0f2778ea3ba4f1
Reiad <reiadwc@gmail.com>
2026-03-23 15:14:33 -0400

    Updated

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -1,7 +1,18 @@
+import WorksPanel from "./WorksPanel"
+import As_thumbnail from "../assets/As_thumbnail.jpg"
 export default function Works(){
     return(
-        <div className="works-container">
-            
+        <div className="works-outline mt-80">
+            <h1 className="heading text-center">Solutions In <span className="sub-heading">Action</span></h1>
+            <div className="works-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12">
+                <WorksPanel img={As_thumbnail} altname="AfterShock Thumbnail" name="AfterShock" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." link="https://aftershock.onrender.com/" />
+                <WorksPanel img={As_thumbnail} altname="AfterShock Thumbnail" name="AfterShock" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." link="https://aftershock.onrender.com/" />
+                <WorksPanel img={As_thumbnail} altname="AfterShock Thumbnail" name="AfterShock" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." link="https://aftershock.onrender.com/" />
+                <WorksPanel img={As_thumbnail} altname="AfterShock Thumbnail" name="AfterShock" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." link="https://aftershock.onrender.com/" />
+                <WorksPanel img={As_thumbnail} altname="AfterShock Thumbnail" name="AfterShock" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." link="https://aftershock.onrender.com/" />
+                <WorksPanel img={As_thumbnail} altname="AfterShock Thumbnail" name="AfterShock" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." link="https://aftershock.onrender.com/" />
+
+            </div>
         </div>
     )
 }
---
+++ src/Components/WorksPanel.jsx
@@ -0,0 +1,10 @@
+export default function WorksPanel({img, altname, name, content, link}){
+    return (
+        <div className="works-panel flex flex-col align-center justify-center gap-4 rounded-lg p-10 text-center"> 
+            <img src={img} alt={altname} className="rounded-lg bg-blue-500"/>
+            <div className="project-title card-title">{name}</div>
+            <div className="project-content card-content">{content}</div>
+            <a href={link} target="_blank" rel="noreferrer" className="btn self-center">View Project</a>
+        </div>
+    )
+}
